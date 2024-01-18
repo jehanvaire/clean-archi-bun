5f02a0d7d6880d1e919fef2a543897da8738f6e0
adrien <adrien.bony38@gmail.com>
2024-01-18 14:18:22 +0100

    🩹 fix body requete client

--- server/src/infrastructure/routes/clientRoutes.ts
+++ server/src/infrastructure/routes/clientRoutes.ts
@@ -3,77 +3,77 @@ import * as Usecases from '../../business/usecases';
 import * as Repositories from '../../infrastructure/repositories';
 
 export function clientRoutes(app: Elysia) {
-    app.post("/client", (body: any) => {
+  app.post("/client", (req: any) => {
 
-        const { nom, prenom, mail } = body.query as { nom: string, prenom: string, mail: string };
-      
-        const storagePort = new Repositories.ClientStorage('./resources/clients.json');
-        // const loggerPort = new Repositories.LoggerRepository();
-        const usecase = new Usecases.AddClient(storagePort);
-        const result = usecase.execute({ nom, prenom, mail });
-      
-        if (result.success) {
-          // return json result
-          return result.value;
-        } else {
-          // return error
-          return result.error;
-        }
-      
-      });
-      
-      app.get("/clients", () => {
-      
-        const storagePort = new Repositories.ClientStorage('./resources/clients.json');
-        // const loggerPort = new Repositories.LoggerRepository();
-        const usecase = new Usecases.GetClients(storagePort);
-        const result = usecase.execute();
-      
-        if (result.success) {
-          // return json result
-          return result.value;
-        } else {
-          // return error
-          return result.error;
-        }
-      
-      });
-      
-      app.put("/client", (body: any) => {
-      
-        const { id, nom, prenom, mail } = body.query as { id: string, nom: string, prenom: string, mail: string };
-      
-        const storagePort = new Repositories.ClientStorage('./resources/clients.json');
-        // const loggerPort = new Repositories.LoggerRepository();
-        const usecase = new Usecases.UpdateClient(storagePort);
-        const result = usecase.execute({ id, nom, prenom, mail });
-      
-        if (result.success) {
-          // return json result
-          return result.value;
-        } else {
-          // return error
-          return result.error;
-        }
-      
-      });
-      
-      app.delete("/client", (body: any) => {
-      
-        const { id } = body.query as { id: string };
-      
-        const storagePort = new Repositories.ClientStorage('./resources/clients.json');
-        // const loggerPort = new Repositories.LoggerRepository();
-        const usecase = new Usecases.DeleteClient(storagePort);
-        const result = usecase.execute(id);
-      
-        if (result.success) {
-          // return json result
-          return result.value;
-        } else {
-          // return error
-          return result.error;
-        }
-      
-      });
+    const { nom, prenom, mail } = req.body as { nom: string, prenom: string, mail: string };
+
+    const storagePort = new Repositories.ClientStorage('./resources/clients.json');
+    // const loggerPort = new Repositories.LoggerRepository();
+    const usecase = new Usecases.AddClient(storagePort);
+    const result = usecase.execute({ nom, prenom, mail });
+
+    if (result.success) {
+      // return json result
+      return result.value;
+    } else {
+      // return error
+      return result.error;
+    }
+
+  });
+
+  app.get("/clients", () => {
+
+    const storagePort = new Repositories.ClientStorage('./resources/clients.json');
+    // const loggerPort = new Repositories.LoggerRepository();
+    const usecase = new Usecases.GetClients(storagePort);
+    const result = usecase.execute();
+
+    if (result.success) {
+      // return json result
+      return result.value;
+    } else {
+      // return error
+      return result.error;
+    }
+
+  });
+
+  app.put("/client", (req: any) => {
+
+    const { id, nom, prenom, mail } = req.body as { id: string, nom: string, prenom: string, mail: string };
+
+    const storagePort = new Repositories.ClientStorage('./resources/clients.json');
+    // const loggerPort = new Repositories.LoggerRepository();
+    const usecase = new Usecases.UpdateClient(storagePort);
+    const result = usecase.execute({ id, nom, prenom, mail });
+
+    if (result.success) {
+      // return json result
+      return result.value;
+    } else {
+      // return error
+      return result.error;
+    }
+
+  });
+
+  app.delete("/client", (req: any) => {
+
+    const { id } = req.body as { id: string };
+
+    const storagePort = new Repositories.ClientStorage('./resources/clients.json');
+    // const loggerPort = new Repositories.LoggerRepository();
+    const usecase = new Usecases.DeleteClient(storagePort);
+    const result = usecase.execute(id);
+
+    if (result.success) {
+      // return json result
+      return result.value;
+    } else {
+      // return error
+      return result.error;
+    }
+
+  });
 }
